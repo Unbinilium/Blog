d51909691025a288f7a65e7bf197ee15b2280a52
Unbinilium <unbinilium@outlook.com>
2021-03-14 04:10:36 +0800

    Refine mermaid initialize configuration and js typos

--- themes/unbinilium/source/js/mermaid-loader.js
+++ themes/unbinilium/source/js/mermaid-loader.js
@@ -1,14 +1,66 @@
-function mermaidLoad() {
-    var theme = 'neutral';
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        theme = 'dark';
+var mermaidConfig = {
+    theme: 'neutral',
+    logLevel: 'fatal',
+    securityLevel: 'loose',
+    startOnLoad: true,
+    arrowMarkerAbsolute: false,
+    er: {
+        diagramPadding: 20,
+        layoutDirection: 'TB',
+        minEntityWidth: 100,
+        minEntityHeight: 55,
+        entityPadding: 15,
+        stroke: 'gray',
+        fill: 'honeydew',
+        fontSize: 12,
+        useMaxWidth: true
+    },
+    flowchart: {
+        diagramPadding: 8,
+        htmlLabels: true,
+        curve: 'linear'
+    },
+    sequence: {
+        diagramMarginX: 50,
+        diagramMarginY: 10,
+        actorMargin: 50,
+        width: 130,
+        height: 30,
+        boxMargin: 10,
+        boxTextMargin: 5,
+        noteMargin: 10,
+        messageMargin: 35,
+        messageAlign: 'center',
+        mirrorActors: true,
+        bottomMarginAdj: 1,
+        useMaxWidth: true,
+        rightAngles: false,
+        showSequenceNumbers: false
+    },
+    gantt: {
+        titleTopMargin: 25,
+        barHeight: 25,
+        barGap: 4,
+        topPadding: 50,
+        leftPadding: 75,
+        gridLineStartPadding: 35,
+        fontSize: 11,
+        fontFamily: '"Menlo", "Meslo LG", monospace',
+        numberSectionStyles: 4,
+        axisFormat: '%Y-%m-%d'
     }
-    mermaid.initialize({ securitylevel: 'loose', theme: theme, themeVariables: { fontFamily: '"Menlo", "Meslo LG", monospace', fontSize: '9px' } });
+};
+
+function mermaidLoaded() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        mermaidConfig['theme'] = 'dark';
+    }
+    mermaid.initialize(mermaidConfig);
 }
 
 (function () {
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js';
-    script.setAttribute('onload', 'mermaidLoad()')
+    script.setAttribute('onload', 'mermaidLoaded()')
     document.head.appendChild(script);
 })();
