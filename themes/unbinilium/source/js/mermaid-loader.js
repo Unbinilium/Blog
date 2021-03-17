1eb2ed5dd2882b1415e380eb0d7efdda0088c403
Unbinilium <unbinilium@outlook.com>
2021-03-17 23:23:29 +0800

    Refactor mermaidjs rendering for color-scheme changes (disables {init})

--- themes/unbinilium/source/js/mermaid-loader.js
+++ themes/unbinilium/source/js/mermaid-loader.js
@@ -1,37 +1,24 @@
-var mermaidConfig = {
-    theme: 'neutral',
-    securityLevel: 'loose',
-    fontFamily: '"Menlo", "Meslo LG", monospace',
-    er: {
-        minEntityWidth: 100,
-        minEntityHeight: 55
-    },
-    sequence: {
-        width: 130,
-        height: 30
-    },
-    gantt: {
-        barHeight: 25,
-        barGap: 4
-    }
-};
+var mermaidTheme = 'neutral';
 
 async function mermaidLoaded() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        mermaidConfig['theme'] = 'dark';
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) { mermaidTheme = 'dark'; }
+    else { mermaidTheme = 'neutral'; }
+    var mermaidContainer = document.getElementsByClassName('mermaid-container');
+    for (var i = 0; i != mermaidContainer.length; ++i) {
+        var mermaidSrc = mermaidContainer[i].firstElementChild;
+        const mermaidOutId = mermaidSrc.id.replace('src', 'out');
+        const mermaidThemeConfig = '%%{init:{ \'theme\':\'' + mermaidTheme + '\'}}%%\n';
+        mermaid.mermaidAPI.render(mermaidOutId, mermaidThemeConfig + mermaidSrc.textContent, async function (svgCode) {
+            mermaidSrc.insertAdjacentHTML('afterend', svgCode);
+        });
     }
-    mermaid.initialize(mermaidConfig);
 }
 
-async function mermaidReload() {
-    if (document.querySelectorAll('[id^=mermaid-]').length != 0) location.reload(false);
-}
-
-if (document.getElementsByClassName('mermaid').length != 0) {
+if (document.getElementsByClassName('mermaid-container').length != 0) {
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js';
     script.setAttribute('onload', 'mermaidLoaded();')
     document.head.appendChild(script);
 }
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', mermaidReload);
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', mermaidLoaded);
